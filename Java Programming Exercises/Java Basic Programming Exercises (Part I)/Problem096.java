db7832af5d681c057b9650ec93d880a9be2dc71e
Avro <42869374+ItsTHEAvro@users.noreply.github.com>
2022-10-04 23:26:38 +0600

    Problem 96

---
+++ Java Programming Exercises/Java Basic Programming Exercises (Part I)/Problem096.java
@@ -0,0 +1,25 @@
+// Author: Jyotirmoy Avro
+
+import java.util.Arrays;
+
+public class Problem096 {
+    public static void main(String[] args) {
+        int[] arr = {20, 10, 3, 5, 2, 6, 7, 1};
+        boolean found10 = false, res = false;
+        int i = 0;
+        for(i = 0; i < arr.length-1; i++)
+        {
+            if(arr[i] == 10) 
+            {
+                found10 = true;
+                break;
+            }
+        }
+        for(; i < arr.length-1; i++)
+        {
+            if(arr[i] == 20) res = true;
+        }
+        System.out.println("Original array: " + Arrays.toString(arr));
+        System.out.println(res);
+    }
+}

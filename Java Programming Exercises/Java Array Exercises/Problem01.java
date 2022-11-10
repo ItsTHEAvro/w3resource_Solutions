7f9b5dfd3cd63fe7c28094d4c686e919601f4a2f
Avro <42869374+ItsTHEAvro@users.noreply.github.com>
2022-11-10 23:39:40 +0600

    Java Array Exercises

---
+++ Java Programming Exercises/Java Array Exercises/Problem01.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+// Author: Jyotirmoy Avro
+
+public class Problem01 {
+    public static void main(String[] args) {
+        int[] arr1 = { 56, 83, 23, 42, 43, 52, 5, 7, 78 };
+        System.out.println("Original array:\n" + Arrays.toString(arr1));
+        Arrays.sort(arr1);
+        System.out.println("Sorted array: \n" + Arrays.toString(arr1));
+
+        System.out.println();
+
+        String[] arr2 = { "Java", "PHP", "Python", "C++", "C#", "C", "Perl", "Kotlin" };
+        System.out.println("Original array:\n" + Arrays.toString(arr2));
+        Arrays.sort(arr2);
+        System.out.println("Sorted array:\n" + Arrays.toString(arr2));
+    }
+}
